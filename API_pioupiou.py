1f1928c8f9e835f6004c0b6dc8706f2a2f3efa81
crobric <ced.deymier@gmail.com>
2020-03-20 19:42:06 +0100

    PiouPiou data collection and dataframe creation enhanced

--- API_pioupiou.py
+++ API_pioupiou.py
@@ -5,20 +5,33 @@ import requests
 import datetime
 data=pd.DataFrame()
 
-response = requests.get("http://api.pioupiou.fr/v1/archive/296?start=last-hour&stop=now")
-apidata=response.json()['data']
-apicolumns=response.json()['legend']
 
-data=pd.DataFrame(apidata,columns=apicolumns)
+######Pïoupiou API:http://developers.pioupiou.fr/api/archive/
+data=pd.DataFrame()
+data_int=pd.DataFrame()
+
+#Identify the months and year that will be queried
+month_query=[5,6,7,8,9]
+year_query=[2019]
+
+#Run loop beacause the API can only be queried for 1 month
+for j in year_query:
+    for i in month_query:
+        response = requests.get("http://api.pioupiou.fr/v1/archive/296?start={}-{}&stop={}-{}".format(j,i,j,i+1))
+        apidata=response.json()['data']
+        apicolumns=response.json()['legend']
+
+        data_int=pd.DataFrame(apidata,columns=apicolumns)
+        data=data.append(data_int)
 
 
-#data['time'] = data['time'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%dT%H:%M:%S.%fZ')
+#Make all date time items in usable format
 data['datetime'] =pd.to_datetime(data['time'], format='%Y-%m-%dT%H:%M:%S.%fZ')
 data['date']=data['datetime'].dt.date
 data['time']=data['datetime'].dt.time
 
+#Calculate the time difference before the time when we want to assess the thermal wind
 timenow=datetime.datetime.now()
 data['time_before_detection']=timenow-data['datetime']
 
-data
-print(response.json())
+
